7cd2cf9267aa7af17f91dee2ce5dc03dfab25307
Demon <demonsingur@gmail.com>
2017-04-17 18:37:54 +0000

    client: js: initialize Calendar input

---
+++ client/js/index.js
@@ -0,0 +1,8 @@
+var dateSelector = new Calendar({
+    input: document.querySelector('#date'),
+    language: {
+        month: ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'],
+        weekday: ['Luni', 'Marți', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă', 'Duminică'],
+        w : ['L', 'M', 'M', 'J', 'V', 'S', 'D']
+    }
+});
