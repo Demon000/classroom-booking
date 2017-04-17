ec5e4b8100caaca9b3711f10b38e465384c22694
Demon <demonsingur@gmail.com>
2017-04-17 18:44:06 +0000

    client: add initial Selector structure

---
+++ client/js/Selector.js
@@ -0,0 +1,11 @@
+(function() {
+    function Selector(container, options) {
+        var s = this;
+        Emitter.mixin(s);
+
+        var active;
+        var options = {};
+    }
+    
+    window.Selector = Selector;
+})();
