c52c94332e330b47e119a9a529f1637f443e574f
Demon <demonsingur@gmail.com>
2017-04-18 18:43:18 +0000

    client: js: move objects to an app element

--- client/js/index.js
+++ client/js/index.js
@@ -1,17 +1,18 @@
-var dateSelector = new Calendar({
-    input: document.querySelector('#date'),
-    language: {
-        month: ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'],
-        weekday: ['Luni', 'Marți', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă', 'Duminică'],
-        w : ['L', 'M', 'M', 'J', 'V', 'S', 'D']
-    }
-});
-var roomSelector = new Selector({
-    container: document.querySelector('#rooms'),
-    options: ['25', '31', '7']
-});
-var error = new Erroneous({
-    container: document.querySelector('#error'),
-    timed: true,
-    duration: 10000
-});
+var app = {
+    dateSelector: new Calendar({
+        input: document.querySelector('#date'),
+        language: {
+            month: ['Ianuarie', 'Februarie', 'Martie', 'Aprilie', 'Mai', 'Iunie', 'Iulie', 'August', 'Septembrie', 'Octombrie', 'Noiembrie', 'Decembrie'],
+            weekday: ['Luni', 'Marți', 'Miercuri', 'Joi', 'Vineri', 'Sâmbătă', 'Duminică'],
+            w : ['L', 'M', 'M', 'J', 'V', 'S', 'D']
+        }
+    }),
+    roomSelector: new Selector({
+        container: document.querySelector('#rooms')
+    }),
+    error: new Erroneous({
+        container: document.querySelector('#error'),
+        timed: true,
+        duration: 10000
+    })
+};
