7256fc7b905060a90d89fb58f6e4f00d2bed38f5
Demon000 <demonsingur@gmail.com>
2017-04-20 20:39:12 +0300

    server: api: add events GET endpoint

--- server/api.js
+++ server/api.js
@@ -12,8 +12,27 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/rooms', (req, res, next) => {
-    let data = config.rooms;
-    res.json(data);
+	let data = config.rooms;
+	res.json(data);
+});
+router.get('/events' , (req, res, next) => {
+	let q = req.query;
+	let view = db.get('events');
+	if(q.room) {
+		view.filter({room: q.room});
+	}
+	if(q.year) {
+		view.filter({year: q.year});
+	}
+	if(q.month) {
+		view.filter({month: q.month});
+	}
+	if(q.day) {
+		view.filter({day: q.day});
+	}
+	let data = view.value();
+	console.log(q, data);
+	res.json(data);
 });
 
 module.exports = router;
