5981c39ae657fba78ea66547a8f0080adf819f85
Demon000 <demonsingur@gmail.com>
2017-04-20 20:41:07 +0300

    server: api: add events POST endpoint

--- server/api.js
+++ server/api.js
@@ -34,5 +34,42 @@ router.get('/events' , (req, res, next) => {
 	console.log(q, data);
 	res.json(data);
 });
+router.post('/events', (req, res, next) => {
+	let b = req.body;
+
+	if(b.password != config.password) {
+		return next(new Error('INVPASS'));
+	}
+
+	let sameHour = db.get('events').find({
+		room: b.room,
+		year: b.year,
+		month: b.month,
+		day: b.day,
+		hour: b.hour
+	}).value();
+
+	if(sameHour) {
+		return next(new Error('EVADDCON'));
+	}
+
+	db.get('events').push({
+		room: b.room,
+		year: b.year,
+		month: b.month,
+		day: b.day,
+		hour: b.hour,
+		name: b.name,
+		description: b.description
+	}).write();
+
+	let sameDay = db.get('events').filter({
+		room: b.room,
+		year: b.year,
+		month: b.month,
+		day: b.day
+	}).sortBy('hour').value();
+	res.json(sameDay);
+});
 
 module.exports = router;
