d89b8187e5eb67c702378cada5902eac0d4a1823
Demon000 <demonsingur@gmail.com>
2017-04-20 20:34:35 +0300

    server: use body parser

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const config = require('./config');
 
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
 app.use(express.static('client'));
+app.use(bodyParser.json());
 
 const api = require('./server/api');
 app.use('/api', api);
