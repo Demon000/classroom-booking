f9c9e86f016f777d1c7f0f599782a290f351e705
Demon000 <demonsingur@gmail.com>
2017-04-21 15:26:24 +0300

    use reviver to turn numbers into strings

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('client'));
-app.use(bodyParser.json());
+app.use(bodyParser.json({
+	reviver(key, value) {
+		return typeof value == 'number' ? value.toString() : value;
+	}
+}));
 
 const api = require('./server/api');
 app.use('/api', api);
