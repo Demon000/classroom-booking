Demon000/classroom-booking

commit 481207a4f09b6e935756dac5d5f4a8547e073d78

server: move number to string reviver to the specific path it's needed since we might not needed this globally

index.js
@@ -7,11 +7,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('client'));
-app.use(bodyParser.json({
-	reviver(key, value) {
-		return typeof value == 'number' ? value.toString() : value;
-	}
-}));
+app.use(bodyParser.json();
 
 const api = require('./server/api');
 app.use('/api', api);

server/api.js
@@ -24,6 +24,12 @@ router.get('/events' , (req, res, next) => {
 router.post('/events', (req, res, next) => {
 	let b = req.body;
 
+	for(var i in b) {
+		if(typeof b[i] == 'number') {
+			b[i] = b[i].toString();
+		}		
+	}
+
 	if(b.password != config.password) {
 		return next(new Error('INVPASS'));
 	}

commit bc56f9581b33cea7f6ba0a86d0a1cef2b82940ac

server: fix syntax error introduced by 481207a4f09b6e935756dac5d5f4a8547e073d78

index.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 app.use(express.static('client'));
-app.use(bodyParser.json();
+app.use(bodyParser.json());
 
 const api = require('./server/api');
 app.use('/api', api);