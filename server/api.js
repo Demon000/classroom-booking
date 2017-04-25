Demon000/classroom-booking

commit 00ac4d8e00348449c1096c322659377ec3591cd0

server: api: add validate function

server/api.js
@@ -47,8 +47,23 @@ function sanitize(req, res, next) {
 	}
 	next();
 }
-
-router.post('/events', authorize, sanitize, (req, res, next) => {
+function validate(req, res, next) {
+	let b = req.body;
+	let valid = true;
+	if(config.rooms.indexOf(b.room) == -1) {
+		valid = false;
+	}
+	const numeric = ['year', 'month', 'day', hour];
+	valid = numeric.every(i => validator.isNumeric(b[i]));
+	if(b.hour < 7 || b.hour > 20) {
+		valid = false;
+	}
+	if(!valid) {
+		return next(new Error('INVDATA'));
+	}
+	next();
+}
+router.post('/events', authorize, sanitize, validate, (req, res, next) => {
 	let b = req.body;
 	console.log(b);
 	let sameHour = db.get('events').find({

commit 85bcf2ed6864b88a4c7c2de014d3ae4ddfc5b621

server: api: fix syntax error introduced by 00ac4d8e00348449c1096c322659377ec3591cd0

server/api.js
@@ -53,7 +53,7 @@ function validate(req, res, next) {
 	if(config.rooms.indexOf(b.room) == -1) {
 		valid = false;
 	}
-	const numeric = ['year', 'month', 'day', hour];
+	const numeric = ['year', 'month', 'day', 'hour'];
 	valid = numeric.every(i => validator.isNumeric(b[i]));
 	if(b.hour < 7 || b.hour > 20) {
 		valid = false;