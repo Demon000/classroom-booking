317e984dd02e6c0fdf33fce6f0b3ae29ef8185a3
Demon <demonsingur@gmail.com>
2017-05-16 08:24:32 +0000

    index: render index.ejs at / using ejs view engine

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static('client'));
 app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 
 const api = require('./server/api');
 app.use('/api', api);
+
+app.get('/', function(req, res) {
+    res.render('client/index');
+});
 
 const error = require('./server/error');
 app.use(error);
