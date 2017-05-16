55d0324106659f9f5a71ee763b60875da9edbf1c
Demon <demonsingur@gmail.com>
2017-05-16 08:40:20 +0000

    index: use absolute path for the client directory

--- index.js
+++ index.js
@@ -6,15 +6,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.static('client'));
+
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
 
 const api = require('./server/api');
 app.use('/api', api);
 
+const path = require('path');
+const clientDir = path.join(__dirname, 'client');
+app.use(express.static(clientDir));
+
 app.get('/', function(req, res) {
-    res.render('client/index');
+    res.render(clientDir + '/index');
 });
 
 const error = require('./server/error');
